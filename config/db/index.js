142268cc3dd22bde35d911714ac51b3cf9c49a41
Your Name <you@example.com>
2020-11-13 17:08:54 +0700

    first commit

--- config/db/index.js
+++ config/db/index.js
@@ -3,6 +3,7 @@ const MONGOURL =
   "mongodb+srv://quanghieu:quanghieu@cluster0.ul1hj.mongodb.net/UsersList?retryWrites=true&w=majority";
 const MONGOURL1 = "mongodb://localhost/UsersList";
 async function connect() {
+  console.log(process.env.MONGO_URI);
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
