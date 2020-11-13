1112d9b5f6df100f114e29ce6665ee462a5e58f2
Your Name <you@example.com>
2020-11-13 22:50:04 +0700

    firts commit

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const db = require("./config/db/index");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 // app.set("views", "views")
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 db.connect();
 
 const userRouter = require("./Routes/User");
